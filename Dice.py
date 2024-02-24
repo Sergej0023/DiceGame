6f02210d9a1746363c3386f98197b322da9b2e1a
Daniel <danielholderik@gmail.com>
2024-02-24 21:21:04 +0100

    Started Dice Classes

--- Dice.py
+++ Dice.py
@@ -1,2 +1,18 @@
-value = 888888888888888888888888888888888
+from enum import Enum
+import random
 
+class Dice(Enum):
+        ONE = 1
+        TWO = 2
+        THREE = 3
+        FOUR = 4
+        FIVE = 5
+        SIX = 6
+
+        def __init__ (self):
+            pass
+
+        def rollDice(self):
+              diceRoll = random.choice(list(Dice))
+              return diceRoll
+              
--- DiceHand.py
+++ DiceHand.py
@@ -1,1 +1,11 @@
-# ajbdaskjbd
+class DiceHand:
+
+    def __init__(self):
+        self.Dice_Value = 0
+
+    def addRoll (self, Dice):
+
+        current_dice_rolls = 0
+        dice_roll = Dice.rollDice
+        if dice_roll > 1:
+            dice_roll += current_dice_rolls
