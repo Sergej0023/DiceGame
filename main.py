a5e9ef3498c16978dd32ea1a9f79450b96068220
Daniel <danielholderik@gmail.com>
2024-02-24 21:44:02 +0100

    menus

--- main.py
+++ main.py
@@ -15,3 +15,31 @@ if __name__ == '__main__':
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 #testing  
+main_menu = """
+│ - - - - - - - - - - - - - - - │
+│           Pig Game            │
+│ - - - - - - - - - - - - - - - │
+│ 1. Play versus bot........... │
+│ 2. Play with another player.. │
+│ 3. Show highscores........... │
+│ 4. Rules..................... │
+│ 5. Quit...................... │
+│ - - - - - - - - - - - - - - - │
+
+Enter your choice (1 to 5):
+"""
+
+bot_menu = """
+│ - - - - - - - - - - - - - - - │
+│      Select Difficulty        │
+│ - - - - - - - - - - - - - - - │
+│ 1. Easy...................... │
+│ 2. Medium.................... │
+│ 3. Hard...................... │
+│ - - - - - - - - - - - - - - - │
+
+Enter your choice (1 to 3):
+"""
+
+print(main_menu)
+print(bot_menu)
