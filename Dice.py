303e011c95fcff05bba478c4914a7cd6e4cb35cf
Sergej0023 <160471585+Sergej0023@users.noreply.github.com>
2024-02-25 17:35:49 +0100

    Update Dice.py

--- Dice.py
+++ Dice.py
@@ -2,16 +2,14 @@ from enum import Enum
 import random
 
 class Dice(Enum):
-        ONE = 1
-        TWO = 2
-        THREE = 3
-        FOUR = 4
-        FIVE = 5
-        SIX = 6
+    ONE = 1
+    TWO = 2
+    THREE = 3
+    FOUR = 4
+    FIVE = 5
+    SIX = 6
 
-        def __init__ (self):
-            pass
-
-        def rollDice(self):
-              diceRoll = random.choice(list(Dice))
-              return diceRoll
+    @staticmethod
+    def rollDice():
+        diceRoll = random.choice(list(Dice))
+        return diceRoll
