e1ef6048259f6e0c61105af2bf0f3282c4ec16a2
Zaskia de Chazal <161013426+zdechazal@users.noreply.github.com>
2024-02-25 08:16:21 +0100

    Update Game.py

--- Game.py
+++ Game.py
@@ -1,9 +1,10 @@
 import Dice
+import Player
 
 class Game:
     def __init__(self, player1, player2):
-        self.player1 = player1
-        self.player2 = player2
+        self.player1 = Player(player1)
+        self.player2 = Player(player2)
         self.dice = Dice()
         self.player1_total = 0
         self.player2_total = 0
