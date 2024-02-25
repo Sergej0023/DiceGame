00fb412d6522e7c30db444d0ee2c274429890b11
Zaskia de Chazal <zaskia.de_chazal0001@stud.hkr.se>
2024-02-25 07:49:21 +0100

    First attempt at the intelligence

--- Game.py
+++ Game.py
@@ -1,2 +1,9 @@
+import Dice
+
 class Game:
-    pass
+    def __init__(self, player1, player2):
+        self.player1 = player1
+        self.player2 = player2
+        self.dice = Dice()
+        self.player1_total = 0
+        self.player2_total = 0
--- Intelligence.py
+++ Intelligence.py
@@ -1,2 +1,16 @@
 class Intelligence:
-    pass
+    def  __init__(self,strength):
+        self.strength = strength
+
+
+    def rollDecision(self, turnTotal):
+        match self.strength:
+            case "Easy":
+                return "roll" if turnTotal < 20 else "hold" #From wikipedia, gives an 8% disadvantage
+            case "medium":
+                return "roll" if turnTotal < 25 else "hold" #From wikipedia, those are all 
+            case "hard":
+                return "roll" if turnTotal < 15 else "hold" #To be tweaked still
+            
+            
+        
