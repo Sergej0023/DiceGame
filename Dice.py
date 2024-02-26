3e31d2791d0ec3c284f820919ce2d35592cc0333
Daniel <danielholderik@gmail.com>
2024-02-26 17:46:39 +0100

    Dice enum fix(?)

--- Dice.py
+++ Dice.py
@@ -1,13 +1,14 @@
 from enum import Enum
 import random
 
-class Dice(Enum):
-    ONE = 1
-    TWO = 2
-    THREE = 3
-    FOUR = 4
-    FIVE = 5
-    SIX = 6
+class Dice:
+    class DiceEnum (Enum):
+        ONE = 1
+        TWO = 2
+        THREE = 3
+        FOUR = 4
+        FIVE = 5
+        SIX = 6
 
     @staticmethod
     def rollDice():
