e873b9f8987c2a0e578f5969b01442d1f170ad62
Sergej0023 <160471585+Sergej0023@users.noreply.github.com>
2024-02-27 12:03:39 +0100

    Update DiceRoll.py
    A Turn class for dice game to work

--- DiceRoll.py
+++ DiceRoll.py
@@ -1,12 +1,36 @@
-class DiceRoll:
+from Dice import Dice
 
+
+# Class of static methods which are actions that can be taken during the turn.
+# It keeps track of scores per turn, temporary score
+class Turn:
     def __init__(self):
-        self.Dice_Value = 0
-        
-    @staticmethod
-    def addRoll (self, Dice):
+        self.score = 0
 
+
+    @staticmethod  # playTurn rolls dice, returns corresponding value
+    def playTurn():
+        diceRollOne = 1
         current_dice_rolls = 0
-        dice_roll = Dice.rollDice
-        if dice_roll > 1:
-            dice_roll += current_dice_rolls
+        dice_roll = Dice.rollDice()
+
+        if dice_roll != diceRollOne:
+            current_dice_rolls += dice_roll
+        return current_dice_rolls
+
+
+    @staticmethod  # endTurn is a choice taken by a player if they want to end the Turn
+    def anotherTurn(choice):
+        stopTurn = "Stop"
+        return False if choice == stopTurn else True
+
+
+    @staticmethod  # skipTurn returns True if turnScore is 0
+    def skipTurn(turnScore):
+        resetPoints = 0
+        return True if turnScore == resetPoints else False
+
+
+    @staticmethod  # winGame returns True if score reaches maxScore
+    def winGame(score, maxScore):
+        return True if score >= maxScore else False
