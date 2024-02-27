425be962f6aa69fab7eab55b15c5c3cbbcd4753d
Sergej0023 <160471585+Sergej0023@users.noreply.github.com>
2024-02-27 12:03:14 +0100

    Update Game.py V1
    First itteration of Functional Pig Dice game

--- Game.py
+++ Game.py
@@ -1,9 +1,54 @@
-import  Player, Dice
+from Player import Player
+from Turn import Turn
+from Intelligence import Intelligence
+
 
 class Game:
-    def __init__(self, player1, player2):
-        self.player1 = Player(player1)
-        self.player2 = Player(player2)
-        self.dice = Dice()
-        self.player1_total = 0
-        self.player2_total = 0
+    def __init__(self, username, maxScore):
+        self.playerOne = Player(str(username))  # enforces string on player name
+        self.playerTwo = Player(str("Player 2"))
+        self.maxScore = maxScore
+
+
+    def pig(self):
+        while True:
+            P1 = self.player(self.playerOne)
+            if P1 == 1:
+                print(f"{self.playerOne.username} wins")
+                break
+
+            P2 = self.player(self.playerTwo)
+            if P2 == 1:
+                print(f"{self.playerTwo.username} wins")
+                break
+
+
+    def player(self, player):
+        turn = Turn()
+
+        turnScore = turn.score
+        playerScore = self.playerOne.score
+
+        while True:
+            dice = Turn.playTurn()
+
+            print(playerScore)
+            print(dice)
+
+            if Turn.skipTurn(dice):
+                print("LOST ROUND!!!")
+                return
+
+            newPlayerScore = turnScore + dice
+            checkWin = Turn.winGame(newPlayerScore, self.maxScore)
+
+            if checkWin is True:
+                return 1  # wins the game
+
+            anotherTurn = Turn.anotherTurn(input("Another turn? "))  # change input from different place
+            if anotherTurn is False:
+                playerScore += turnScore
+                return
+
+            turnScore += dice
+
