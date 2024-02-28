48ff7b4a73165c05a0161b0096f2d9aaf76d5f9d
Daniel <danielholderik@gmail.com>
2024-02-28 23:43:21 +0100

    high_score start

--- HighScore.py
+++ HighScore.py
@@ -1,2 +1,20 @@
+import pickle
+
 class HighScore:
-    pass
+    def __init__(self):
+        self.all_player_list = []
+
+    @staticmethod
+    def add_player_to_list(player, all_player_list):
+        all_player_list.append(player)
+   
+    @staticmethod  
+    def save_scores (self, player, filepath = "players_highscores.pkl"):
+        self.all_player_list.sort(key = lambda player: player.turns)    # sorting player depending on their turns
+        with open (filepath, "wb") as player_file:                      # dumping into binary file
+            pickle.dump(self.all_player_list, player_file)
+
+    @staticmethod
+    def load_scores (self, filepath = "players_highscores.pkl"):
+        with open (filepath, "rb") as player_file:  
+            
