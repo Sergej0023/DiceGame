3e6829177240228459fdf37113218267b20c017e
Sergej0023 <160471585+Sergej0023@users.noreply.github.com>
2024-03-05 20:34:04 +0100

    Rework player Class
    Becomes parent class

--- Player.py
+++ Player.py
@@ -1,39 +1,29 @@
 # Add interface for players
 class Player:
-    def __init__(self, username, score = 0, turns = 0):
-        self.username = username
-        self.score = score
-        self.turns = turns
+    def __init__(self):
+        self.username = None
+        self.score = 0
         self.runningScore = 0
-        self.scoreHistory = []
-        self.gamesPlayed = 0
-
-
-
-    def changeUsername(self, changedUsername):
-        self.username = changedUsername
-
-
-    def cheat(self, score):
-        self.score = score
 
 
     def updateScore(self, newScore):
         self.score += newScore
 
-    @staticmethod
-    def resetTurns (self, turns):
-        self.turns = turns
-    
-    @staticmethod
-    def resetScore (self, score):   
-        self.score = score
-
-    def scoreReset (self):
-        self.scoreHistory.append([f"Scored {self.score} in {self.turns} turns"])
-        self.gamesPlayed += 1               # Call this at end of each game to
-        self.resetScore(self, score = 0)    # reset the scores and keep track of
-        self.resetTurns(self, turns = 0)    # games played and previous scores.
 
     def updateRunningScore(self, runningScore):
         self.runningScore = runningScore
+
+
+    @staticmethod  # Helper method for resetGame
+    def resetTurns(self, turns):
+        self.turns = turns
+
+
+    @staticmethod  # Helper method for resetGame
+    def resetScore(self, score):
+        self.score = score
+
+
+    def resetGame(self):
+        self.resetScore(self, 0)  # reset the scores and keep track of
+        self.resetTurns(self, 0)  # games played and previous scores.
