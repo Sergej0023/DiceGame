bd895027a97ddb7db6c7eae712916d467fe13b4d
Sergej0023 <160471585+Sergej0023@users.noreply.github.com>
2024-03-05 20:36:46 +0100

    Rewrite Game.py

--- Game.py
+++ Game.py
@@ -5,41 +5,26 @@ import Difficulty
 
 
 class Game:
-    def __init__(self, playerOne, playerTwo, computer, mode): 
-        self.playerOne = playerOne # enforces string on player name
-        self.maxScore = 25
+    def __init__(self, playerOne, playerTwo):
+        self.playerOne = playerOne  # enforces string on player name
+        self.playerTwo = playerTwo
+        self.maxScore = 50
 
-        #both are "playerTwo". When using which specific methods in DiceRoll with do check with: 
-        # if type(self.playerTwo) is Player: 
-        if mode == "p":
-            self.playerTwo = playerTwo
-        elif mode == "c":
-            self.playerTwo = computer
 
-    
     def pig(self):
         while True:
             P1 = Turn.playerTurn(self.playerOne, self.maxScore)
             if P1 == "w":
-                self.playerOne.updateScore(0)
-                self.playerTwo.updateScore(0)
                 print(f"\n{self.playerOne.username} wins!!")  # Testing purposes
                 break
-            elif P1 == "q":     
-                self.playerOne.updateScore(0)
-                self.playerTwo.updateScore(0)
+            elif P1 == "q":
                 break
-            
+
             print("\nNext Player Turn!")
             P2 = Turn.playerTurn(self.playerTwo, self.maxScore)
             if P2 == "w":
-                self.playerOne.updateScore(0)
-                self.playerTwo.updateScore(0)
                 print(f"\n{self.playerTwo.username} wins!!")  # testing purposes
                 break
             elif P2 == "q":
-                self.playerOne.updateScore(0)
-                self.playerTwo.updateScore(0)
                 break
             print("\nNext Player Turn!")
-
