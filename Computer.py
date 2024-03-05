5d433493adf7c03c572f0a1dbedc4d4d64ee61d4
Sergej0023 <160471585+Sergej0023@users.noreply.github.com>
2024-03-05 20:35:21 +0100

    Update Computer.py
    Add Polymorphism

--- Computer.py
+++ Computer.py
@@ -1,45 +1,44 @@
 from Difficulty import Difficulty
+from Player import Player
 
-class Computer:
-    def  __init__(self, difficulty):
+
+class Computer(Player):
+    def __init__(self, difficulty):
+        super().__init__()
         self.username = "Bob the AI"
         self.difficulty = difficulty
-        self.runningScore = 0
-        self.score = 0
 
 
     def rollDecision(self):
         match self.difficulty:
-            case "1":#Easy level           
-                 return self.decision(20) #From wikipedia, gives an 8% disadvantage
-            case "2": #Medium level
-                return self.decision(25) #From wikipedia, those are all 
-            case "3": #Hard level
-                return self.decision(15) #To be tweaked still
-            
+            case "1":  # Easy level
+                return self.decision(20)  # From wikipedia, gives an 8% disadvantage
+            case "2":  # Medium level
+                return self.decision(25)  # From wikipedia, those are all
+            case "3":  # Hard level
+                return self.decision(15)  # To be tweaked still
+
+
     def decision(self, turnTotal):
         return "r" if self.runningScore < turnTotal else "h"
-    
 
-    
+
     def setDifficulty(self, difficultyLvl):
         values = [member.name for member in Difficulty]
-        if difficultyLvl not in values: raise ValueError("Sorry, the difficulty level does not exist")
-        else: self.difficulty = difficultyLvl
-        
+        if difficultyLvl not in values:
+            raise ValueError("Sorry, the difficulty level does not exist")
+        else:
+            self.difficulty = difficultyLvl
+
+
     def checkDifficulty(self, difficultyLvl):
         values = [member.name for member in Difficulty]
         if difficultyLvl not in values: raise ValueError("Sorry, the difficulty level does not exist")
-            
-        
-            
+
+
     def updateScore(self, score):
         self.score = score
 
-            
+
     def updateRunningScore(self, runningScore):
         self.runningScore = runningScore
-
-    
-
-        
