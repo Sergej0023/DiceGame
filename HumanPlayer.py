0d0a3aa16a49dc1e69ce8ce38aaf55d0908e0001
Sergej0023 <160471585+Sergej0023@users.noreply.github.com>
2024-03-06 18:49:17 +0100

    Tweak

--- HumanPlayer.py
+++ HumanPlayer.py
@@ -1,5 +1,6 @@
 # Add interface for players
 from Player import Player
+from Displays import Displays
 
 
 class HumanPlayer(Player):
@@ -9,6 +10,7 @@ class HumanPlayer(Player):
         self.turns = 0
         self.scoreHistory = []
         self.gamesPlayed = 0
+        self.quit = "Q"
 
 
     def changeUsername(self, username):
@@ -22,3 +24,12 @@ class HumanPlayer(Player):
     def updatePlayerStats(self):
         self.scoreHistory.append([f"Scored {self.score} in {self.turns} turns"])
         self.gamesPlayed += 1  # Call this at end of each game to
+
+
+    def anotherTurn(self):
+        while True:
+            playerDecision = Displays.inputAnotherTurn()
+            if playerDecision.upper() == self.roll or playerDecision.upper() == self.hold:
+                return playerDecision.upper()
+            elif playerDecision.upper() == self.quit:
+                return self.quit
