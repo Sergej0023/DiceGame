9639ff4844d95335c2ae3318f19fe8772a39ec4a
Sergej0023 <160471585+Sergej0023@users.noreply.github.com>
2024-03-07 19:56:19 +0100

    Finished highscore

--- HighScore.py
+++ HighScore.py
@@ -1,46 +1,53 @@
 import pickle
 import os
-from Player import Player
 from HumanPlayer import HumanPlayer
+from displays import Displays
 
 
-class highscore:
-
+class Highscore:
     def __init__(self):
-
-        self.allPlayerList = []
+        self.all_player_list = []
+        self.top_ten = []
         self.filepath = "players_highscores.pkl"
 
+        with open(self.filepath, "a"):  # ensures file exists
+            pass
+
         # Added this so if the list is not empty, it loads. Must be loaded once only, at the very beginning.
-        if os.path.getsize(self.filepath) > 0:
-            self.allPlayerList = self.load_scores()
+        self.load_scores()
 
 
-    def add_player_to_list(self, player):
-        self.allPlayerList.append(player)
+    def add_player_to_list(self, player):  # adds player to the list if it's not already
+        if player not in self.all_player_list:
+            self.all_player_list.append(player)
 
 
-    def save_scores(self):
+    def save_scores(self): # saves all players
         with open(self.filepath, "wb") as playerFile:
-            pickle.dump(self.allPlayerList, playerFile)
+            pickle.dump(self.all_player_list, playerFile)
 
 
-    def load_scores(self):
+    def load_scores(self):  # loads player to list, if empty EOF exception makes empty list
         try:
             with open(self.filepath, "rb") as playerFile:
-                self.allPlayerlist = pickle.load(playerFile)
+                self.all_player_list = pickle.load(playerFile)
 
         except FileNotFoundError:
             print(f"File not found: {self.filepath}. ")
             return []
 
+        except EOFError:
+            self.all_player_list = []
 
-    def display_scores(self):
-        try:
-            self.allPlayerList.sort(key=lambda player: player.turns)
-            for player in self.allPlayerList:
-                print(f"Player name:  {player.name}, Ammount of turns: {player.turns}")
 
-        except TypeError:
-            print("The player list is not properly defined.")
+    def sort_scores(self):  # used to parse top 10 scores
+        if self.all_player_list:  # if list is not empty
+            self.all_player_list.sort(key=lambda player: player.turns, reverse=True)
+            self.top_ten = self.all_player_list[0:10]
 
+
+    def display_scores(self):  # used to print scores
+        self.sort_scores()
+        print("Highscores:")
+        for player in self.top_ten:
+            Displays.print_players_score(player.username, player.turns)
