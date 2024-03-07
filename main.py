019652c4bd1f6c9aa10a308edc6a00de7587f5b9
Sergej0023 <160471585+Sergej0023@users.noreply.github.com>
2024-03-07 16:39:47 +0100

    changed name to snake

--- main.py
+++ main.py
@@ -1,53 +1,50 @@
-from Displays import Displays
+from displays import Displays
 from Game import Game
 from Computer import Computer
-from Computer import Computer
-from HighScore import HighScore
-from Difficulty import Difficulty
+from highscore import highscore
 from HumanPlayer import HumanPlayer
-from Player import Player
 
 
 def main():
-    playerName = input("Welcome to Pig! Enter the name of player one: ")
-    playerOne = HumanPlayer()
-    playerOne.changeUsername(playerName)
+    player_name = input("Welcome to Pig! Enter the name of player one: ")
+    player_one = HumanPlayer()
+    player_one.username = player_name
 
-    HighScore()
+    highscore()
 
     while True:
-        Displays.printMenu()
+        Displays.print_menu()
         choice = input("Enter your choice (1 to 5): ")
 
         match choice:
             case "1":
-                Displays.printBotMenu()
-                difficultyLevel = input("Enter your choice (1 to 4): ")
+                Displays.print_bot_menu()
+                difficulty_level = input("Enter your choice (1 to 4): ")
 
-                if difficultyLevel in {"1", "2", "3"}:
-                    computer = Computer(difficultyLevel)
-                    game = Game(playerOne, computer)  # starts a game with a username and difficulty lvl
+                if difficulty_level in {"1", "2", "3"}:
+                    computer = Computer(difficulty_level)
+                    game = Game(player_one, computer)  # starts a game with a username and difficulty lvl
                     game.pig()
 
-                elif difficultyLevel == "4":
-                    Displays.printMenu()
+                elif difficulty_level == "4":
+                    Displays.print_menu()
 
                 else:
                     print("Enter a valid choice")
 
             case "2":
-                player2Name = input("Enter the name of player two: ")
-                playerTwo = HumanPlayer()
-                playerTwo.changeUsername(player2Name)
+                player_two_name = input("Enter the name of player two: ")
+                player_two = HumanPlayer()
+                player_two.username = player_two_name
 
-                game = Game(playerOne, playerTwo)
+                game = Game(player_one, player_two)
                 game.pig()
 
             case "3":
-                HighScore.displayScores(playerName)
+                highscore.display_scores()
 
             case "4":
-                Displays.printPlayerOptions()
+                Displays.print_player_options()
                 option = input("\nEnter your choice (1 to 3): ")
 
                 match option:
@@ -55,11 +52,11 @@ def main():
                         pass
 
                     case "2":
-                        newUsername = input("What's the new username: ")
-                        playerOne.changeUsername(newUsername)
+                        new_username = input("What's the new username: ")
+                        player_one.username = new_username
 
             case "5":
-                Displays.printRules()
+                Displays.print_rules()
 
             case "6":
                 break
