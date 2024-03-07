9739fa649bdbac6de39021362a458d10241e0d16
Daniel <danielholderik@gmail.com>
2024-03-07 17:43:37 +0100

    DONE

---
+++ test_dice.py
@@ -0,0 +1,18 @@
+import unittest
+from unittest.mock import patch
+from Dice import Dice
+import random
+
+
+class test_dice(unittest.TestCase):
+    
+    @patch('random.choice', return_value = 4)
+    def test_roll_dice(self, mock_choice):
+        dice = Dice()
+
+        roll = dice.roll_dice()
+
+        self.assertEqual(roll, 4)
+       
+if __name__ == '__main__':
+    unittest.main()
